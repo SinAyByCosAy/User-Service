f304e694a2f4b64ba2382b7921c6b32f1b634b40
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-12-01 19:27:11 +0530

    DB configured

--- src/main/java/dev/tanay/userservice/repositories/UserRepository.java
+++ src/main/java/dev/tanay/userservice/repositories/UserRepository.java
@@ -1,10 +1,10 @@
-package dev.tanay.userservice.repositories;
-
-import dev.tanay.userservice.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    public void createUser(User user);
-    public String getPassword(String email);
-    public void deleteUser(Long id);
-}
+//package dev.tanay.userservice.repositories;
+//
+//import dev.tanay.userservice.models.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//    public void createUser(User user);
+//    public String getPassword(String email);
+//    public void deleteUser(Long id);
+//}
--- src/main/java/dev/tanay/userservice/services/UserServiceDB.java
+++ src/main/java/dev/tanay/userservice/services/UserServiceDB.java
@@ -1,15 +1,20 @@
 package dev.tanay.userservice.services;
 
 import dev.tanay.userservice.models.User;
+//import dev.tanay.userservice.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceDB implements UserService{
+//    private UserRepository userRepository;
+//    public UserServiceDB(UserRepository userRepository){
+//        this.userRepository = userRepository;
+//    }
     public void createUser(User user){
-
+//        userRepository.createUser(user);
     }
     public void loginUser(User user){
-
+//        userR
     }
     public void logoutUser(Long id){
 
