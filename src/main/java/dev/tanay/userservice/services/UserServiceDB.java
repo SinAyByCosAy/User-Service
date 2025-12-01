4aa3783e7f17fe49eee4705cb24ba566e8570912
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-12-01 19:18:27 +0530

    Repository setup

---
+++ src/main/java/dev/tanay/userservice/services/UserServiceDB.java
@@ -0,0 +1,17 @@
+package dev.tanay.userservice.services;
+
+import dev.tanay.userservice.models.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceDB implements UserService{
+    public void createUser(User user){
+
+    }
+    public void loginUser(User user){
+
+    }
+    public void logoutUser(Long id){
+
+    }
+}
