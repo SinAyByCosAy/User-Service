e0fd9c21ce473676b5c80775282a68c728ea3843
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-12-02 03:00:07 +0530

    Session Repository

---
+++ src/main/java/dev/tanay/userservice/repositories/SessionRepository.java
@@ -0,0 +1,8 @@
+package dev.tanay.userservice.repositories;
+
+import dev.tanay.userservice.models.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+    public void deleteByToken(String token);
+}
