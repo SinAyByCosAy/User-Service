8c97a6c44cca4ec99aa4a07c41a554bc3563c28e
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-12-01 18:09:59 +0530

    Base setup

---
+++ src/main/java/dev/tanay/userservice/controllers/UserController.java
@@ -0,0 +1,12 @@
+package dev.tanay.userservice.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @PostMapping("/users")
+    public HttpStatus createUser(@RequestBody User user)
+}
---
+++ src/test/java/dev/tanay/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tanay.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
