33652bd08310182cd369f669d315c9d11bd100cc
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-12-09 02:06:40 +0530

    Login route - controller

--- src/main/java/dev/tanay/userservice/controllers/AuthController.java
+++ src/main/java/dev/tanay/userservice/controllers/AuthController.java
@@ -1,16 +1,18 @@
 package dev.tanay.userservice.controllers;
 
+import dev.tanay.userservice.dtos.LoginRequestDto;
 import dev.tanay.userservice.dtos.SignupRequestDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     @PostMapping("/signup")
-    public void signup(SignupRequestDto signupRequestDto){
+    public void signup(@RequestBody SignupRequestDto signupRequestDto){
         System.out.println(signupRequestDto);
+    }
+    @PostMapping("/login")
+    public void login(@RequestBody LoginRequestDto loginRequestDto){
+        System.out.println(loginRequestDto.getEmail() + " " + loginRequestDto.getPassword());
     }
 }
