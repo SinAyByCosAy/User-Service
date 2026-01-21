0252c94458f19e4dce7f076c098c69b767581fa7
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-01-21 18:24:02 +0530

    Session by token only

--- src/main/java/dev/tanay/userservice/repositories/SessionRepository.java
+++ src/main/java/dev/tanay/userservice/repositories/SessionRepository.java
@@ -10,6 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
-    Optional<Session> findSessionByTokenAndIsActiveTrueAndExpiryAtAfter(String token, Instant now);
+    Optional<Session> findSessionByToken(String token);
     void deleteSessionByToken(String token);
 }
