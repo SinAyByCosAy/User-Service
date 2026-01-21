1352a94df72b7e4f3b0e0ed5d28ec0f306388c95
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-01-21 16:58:25 +0530

    Session update

--- src/main/java/dev/tanay/userservice/models/Session.java
+++ src/main/java/dev/tanay/userservice/models/Session.java
@@ -1,12 +1,13 @@
 package dev.tanay.userservice.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
-import java.util.Date;
 
 @Entity
 @Getter
@@ -14,7 +15,8 @@ import java.util.Date;
 public class Session extends BaseModel{
     private String token;
     private Instant expiryAt;
-    private boolean isActive;
+    @Enumerated(EnumType.STRING)
+    private SessionStatus status;
     @ManyToOne
     private User user;
 }
